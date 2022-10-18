bb18e5018c398c4bfc2db6030ddc5e20b47ad102
Po-dev <pofessionalgaming@hotmail.com>
2022-10-18 10:26:33 -0700

    addys fixed

--- boot/boot.cmd
+++ boot/boot.cmd
@@ -1,4 +1,4 @@
-load mmc 0:1 0x50000000 /boot/Image
+load mmc 0:1 0x51000000 /boot/Image
 load mmc 0:1 0x4E000000 /boot/initrd
 load mmc 0:1 0x48000000 /boot/xen
 load mmc 0:1 0x46000000 /boot/sun50i-h616-orangepi-zero2.dtb
@@ -9,7 +9,7 @@ fdt set /chosen \#size-cells <0x2>
 fdt set /chosen xen,xen-bootargs "console=dtuart dtuart=serial0 dom0_mem=128M dom0_max_vcpus=1 bootscrub=0 vwfi=native sched=null  "
 fdt mknod /chosen dom0
 fdt set /chosen/dom0 compatible  "xen,linux-zimage" "xen,multiboot-module" "multiboot,module"
-fdt set /chosen/dom0 reg <0x0 0x50000000 0x0 0x51da1cc0>
+fdt set /chosen/dom0 reg <0x0 0x51000000 0x0 0x52da1cc0>
 fdt set /chosen xen,dom0-bootargs "console=hvc0 earlycon=xen earlyprintk=xen clk_ignore_unused root=/dev/ram0"
 fdt mknod /chosen dom0-ramdisk
 fdt set /chosen/dom0-ramdisk compatible  "xen,linux-initrd" "xen,multiboot-module" "multiboot,module"
